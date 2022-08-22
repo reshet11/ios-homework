9349e1520f7633e20c6579fa0be18aa8e6717097
reshet11 <reshet11@yandex.ru>
2022-08-22 15:31:20 +0300

    update homework15 1.0

--- homework15.playground/Contents.swift
+++ homework15.playground/Contents.swift
@@ -2,24 +2,130 @@ import UIKit
 
 // 1.Создать класс в котором будет 3 проперти мама Папа и дети . Создать subscript и упорядочить их по индексу.
 
+class Family {
+    
+    var mother: String
+    var father: String
+    var children: [String]
+    
+    init(mother: String, father: String, children: [String]) {
+        self.mother = mother
+        self.father = father
+        self.children = children
+    }
+    
+    var count: Int {
+        return 2 + children.count
+    }
+    
+    subscript(index: Int) -> String {
+        switch index {
+        case 0: return mother
+        case 1: return father
+        case 2..<(2 + children.count): return children[index - 2]
+        default: return ""
+        }
+    }
+}
 
+var family = Family(mother: "Olga", father: "Fedor", children: ["Bob", "Alex", "Dmitry", "Egor"])
+
+for i in 0..<family.count {
+    switch i {
+    case 0:
+        print("index \(i) - \(family[i]). This is mother")
+    case 1:
+        print("index \(i) - \(family[i]). This is father")
+    default:
+        print("index \(i) - \(family[i]). This is children")
+    }
+}
 
 // 2.Создать Индекс который будет принимать число умножать его на 100 и делить на 2.
 
+struct Calculation {
+    subscript(index: Double) -> Double {
+        index * 100 / 2
+    }
+}
 
+var calcNumber = Calculation()
+calcNumber[30.5423]
 
 // 3.Создать свой subscript у которого внутри есть оператор if else.
 
+struct Score {
+    
+    subscript(index: Int) -> String {
+        if index == 2 {
+            return "unsatisfactory"
+        } else if index == 3 {
+            return "satisfactory"
+        } else if index == 4 {
+            return "good"
+        } else if index == 5 {
+            return "very good"
+        } else {
+            return "incorrect score"
+        }
+    }
+}
 
+let score = Score()
+score[4]
 
 // 4.Создать класс человек и в нем 3 проперти имя , возраст пол.
 
+class Human {
+    
+    enum Gender {
+        case male, female
+    }
+    
+    var name: String
+    var age: Int
+    var gender: Gender
+    
+    init(name: String, age: Int, gender: Gender) {
+        self.name = name
+        self.age = age
+        self.gender = gender
+    }
+    
+    func info() {
+        print("name - \(name), age - \(age), gender - \(gender)")
+    }
+}
 
+var human = Human(name: "Roman", age: 32, gender: .male)
+human.info()
 
 // 5.Создать ещё 2 класса которые наследуються от класса человек и переопределить методы и свойства.
+// Наследование - это следующая домашка, сделаю это задание после прохождения, пока написал все что знаю по этой теме.
 
+class Children: Human {
+    
+    func children() {
+        print("\(name), \(String(age)) age")
+    }
+}
 
+var child = Children(name: "Nick", age: 8, gender: .male)
 
 // 6.Создать дикшинари который хранит все типы данных которые есть а ключ должен быть только строка (воспользоваться enum для типов данных)
 
+enum TypeDate {
+    case string(String)
+    case character(Character)
+    case integer(Int)
+    case double(Double)
+    case float(Float)
+    case bool(Bool)
+}
 
+let dictTypeDate: Dictionary <String, TypeDate> = ["String": TypeDate.string("строка"),
+                                                   "Character": TypeDate.character("$"),
+                                                   "Integer": TypeDate.integer(123),
+                                                   "Double": TypeDate.double(44.23),
+                                                   "Float": TypeDate.float(1.543267),
+                                                   "bool": TypeDate.bool(true)]
