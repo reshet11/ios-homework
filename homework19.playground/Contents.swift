99abc952eaaff7ea6d8c7a6f87b78a3c84fc0f83
reshet11 <reshet11@yandex.ru>
2022-09-13 15:39:23 +0300

    update homework19 2.0

--- homework19.playground/Contents.swift
+++ homework19.playground/Contents.swift
@@ -7,50 +7,58 @@ import UIKit
 // 3. Сделать так чтоб когда я вызову метод(создайте сами его) - "Sort by mood" то фильмы должны отсортировать по настроению
 // 4. И такой же метод придумать для книг
 
-enum Genre {
-    case horror, drama, comedy, adventure, fantasy
+enum Genre: String {
+    case adventure = "adventure"
+    case comedy = "comedy"
+    case drama = "drama"
+    case fantasy = "fantasy"
+    case horror = "horror"
 }
 
 enum Mood {
     case bad, good, veryGood
 }
 
-class Library {
+struct Book {
     var name: String
     var releaseDate: Int
     var genre: Genre
     var author: String
-    
-    init(name: String, releaseDate: Int, genre: Genre, author: String) {
-        self.name = name
-        self.releaseDate = releaseDate
-        self.genre = genre
-        self.author = author
-    }
+}
+
+struct Movie {
+    var name: String
+    var releaseDate: Int
+    var genre: Genre
+    var author: String
 }
 
 class BookLibrary {
-    var books = [Library]()
+    var books = [Book]()
     
     func addBook(name: String, releaseDate: Int, genre: Genre, author: String) {
-        books.append(Library(name: name, releaseDate: releaseDate, genre: genre, author: author))
+        books.append(Book(name: name, releaseDate: releaseDate, genre: genre, author: author))
         self.books = books.sorted(by: { $0.name < $1.name })
     }
     
-    func sortByMood(_ mood: Mood) {
+    func sortByMood() {
+        self.books = books.sorted(by: { $0.genre.rawValue < $1.genre.rawValue })
+    }
+    
+    func filterByMood(_ mood: Mood) {
         switch mood {
         case .bad:
-            let book: [Library] = books.filter({ $0.genre == .comedy || $0.genre == .fantasy})
+            let book: [Book] = books.filter({ $0.genre == .comedy || $0.genre == .fantasy})
             for value in book {
                 print("bad mood book: \(value.name) - \(value.genre)")
             }
         case .good:
-            let book: [Library] = books.filter({ $0.genre == .adventure})
+            let book: [Book] = books.filter({ $0.genre == .adventure})
             for value in book {
                 print("good mood book: \(value.name) - \(value.genre)")
             }
         case .veryGood:
-            let book: [Library] = books.filter({ $0.genre == .horror || $0.genre == .drama})
+            let book: [Book] = books.filter({ $0.genre == .horror || $0.genre == .drama})
             for value in book {
                 print("very good mood book: \(value.name) - \(value.genre)")
             }
@@ -65,30 +73,35 @@ booksLibrary.addBook(name: "War and Peace", releaseDate: 1873, genre: .drama, au
 booksLibrary.addBook(name: "Woe from wit", releaseDate: 1822, genre: .comedy, author: "Alexander Griboyedov")
 booksLibrary.addBook(name: "Three Musketeers", releaseDate: 1844, genre: .adventure, author: "Alexander Duma")
 
-booksLibrary.sortByMood(.veryGood)
+booksLibrary.sortByMood()
+booksLibrary.filterByMood(.veryGood)
 
 class MovieLibrary {
-    var movies = [Library]()
+    var movies = [Movie]()
     
     func addMovie(name: String, releaseDate: Int, genre: Genre, author: String) {
-        movies.append(Library(name: name, releaseDate: releaseDate, genre: genre, author: author))
+        movies.append(Movie(name: name, releaseDate: releaseDate, genre: genre, author: author))
         self.movies = movies.sorted(by: { $0.name < $1.name })
     }
     
-    func sortByMood(_ mood: Mood) {
+    func sortByMood() {
+        self.movies = movies.sorted(by: { $0.genre.rawValue < $1.genre.rawValue })
+    }
+    
+    func filterByMood(_ mood: Mood) {
         switch mood {
         case .bad:
-            let movie: [Library] = movies.filter({ $0.genre == .comedy || $0.genre == .fantasy})
+            let movie: [Movie] = movies.filter({ $0.genre == .comedy || $0.genre == .fantasy})
             for value in movie {
                 print("bad mood movie: \(value.name) - \(value.genre)")
             }
         case .good:
-            let movie: [Library] = movies.filter({ $0.genre == .adventure})
+            let movie: [Movie] = movies.filter({ $0.genre == .adventure})
             for value in movie {
                 print("good mood movie: \(value.name) - \(value.genre)")
             }
         case .veryGood:
-            let movie: [Library] = movies.filter({ $0.genre == .horror || $0.genre == .drama})
+            let movie: [Movie] = movies.filter({ $0.genre == .horror || $0.genre == .drama})
             for value in movie {
                 print("very good mood movie: \(value.name) - \(value.genre)")
             }
@@ -103,4 +116,5 @@ moviesLibrary.addMovie(name: "Home alone", releaseDate: 1990, genre: .comedy, au
 moviesLibrary.addMovie(name: "The revenant", releaseDate: 2015, genre: .adventure, author: "Mark Smith")
 moviesLibrary.addMovie(name: "The Shawshank redemption", releaseDate: 1994, genre: .drama, author: "Frank Darabont")
 
-moviesLibrary.sortByMood(.bad)
+moviesLibrary.sortByMood()
+moviesLibrary.filterByMood(.bad)
